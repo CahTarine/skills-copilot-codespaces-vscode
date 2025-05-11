f1b3b39cdbceeeb0e77e6422fcf73cfd913e4df5
Camille Tarine <camille.tarine@gmail.com>
2025-05-11 19:27:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,21 @@
+function skillsMember() {
+    const skills = [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "Python",
+        "Django",
+        "Flask",
+        "Java",
+        "Spring Boot",
+        "C++",
+        "C#",
+        "Ruby on Rails",
+    ];
+    
+    return skills;
+}
